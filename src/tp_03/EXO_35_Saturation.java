ddf9375d96ad39cb55304611295a39a9f1f52e31
Laurent <laurent.digitick@hotmail.com>
2020-07-27 11:30:47 +0200

    Sauv TP1

--- src/tp_03/EXO_35_Saturation.java
+++ src/tp_03/EXO_35_Saturation.java
@@ -17,10 +17,33 @@ public class EXO_35_Saturation {
     
     public static int[][] mystere(int[][] matrice) {
        int i, j;
-        
-       for(i=0; i<4; i++){
-           for(j=0; j<6; j++){
+       int nl=matrice.length; 
+       int nc=matrice[0].length;
+       for(i=0; i<nl; i++){
+           for(j=0; j<nc; j++){
                matrice[i][j]= 100- matrice[i][j];
+           }
+       }
+           
+           return matrice;
+    }
+    
+    public static int[][] contraste(int[][] matrice) {
+       int i, j;
+       int n=matrice.length;
+       int m=matrice[0].length; 
+       
+       for(i=0; i<n; i++){
+           for(j=0; j<m; j++){
+              if(matrice[i][j]>75){
+                  matrice[i][j]=100;
+                  
+              }else if(matrice[i][j]>50){
+                  matrice[i][j]=75;
+                  
+              }else{
+                matrice[i][j]=matrice[i][j]/2;
+               }
            }
        }
            
